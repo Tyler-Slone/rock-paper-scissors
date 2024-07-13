81e9f96869bbe69b47c5edb0b4a2e48937a87dd3
Tyler Slone <tyl3rslon3@gmail.com>
2024-07-13 19:08:24 -0400

    Add roundWinner function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ['rock', 'paper', 'scissors']
+const humanScore = 0
+const computerScore = 0
 
 function getComputerChoice(){
    randomChoice = choices[(Math.random() * choices.length) | 0]
@@ -13,6 +15,36 @@ function getHumanChoice(){
     else {
         console.log('Please enter a valid input')
         getHumanChoice();
+    }
+}
+
+function roundWinner() {
+    if (humanChoice === computerChoice){
+        console.log('Tie')
+    }
+    if (humanChoice === 'rock'  && computerChoice === 'paper'){
+        console.log('Computer wins')
+        computerScore++
+    }
+    if (humanChoice === 'rock'  && computerChoice === 'scissors'){
+        console.log('Round won')
+        humanScore++
+    }
+    if (humanChoice === 'paper'  && computerChoice === 'rock'){
+        console.log('Round won')
+        humanScore++
+    }
+    if (humanChoice === 'paper'  && computerChoice === 'scissors'){
+        console.log('Computer wins')
+        computerScore++
+    }
+    if (humanChoice === 'scissors'  && computerChoice === 'rock'){
+        console.log('Computer wins')
+        computerScore++
+    }
+    if (humanChoice === 'scissors'  && computerChoice === 'paper'){
+        console.log('Round won')
+        humanScore++
     }
 }
 
