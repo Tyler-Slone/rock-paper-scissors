af2d57a68dc1436c526c50c873d9ddd8dac38e69
Tyler Slone <tyl3rslon3@gmail.com>
2024-08-24 22:30:22 -0400

    Add logic to play first to five points

--- script.js
+++ script.js
@@ -20,16 +20,20 @@ function getComputerChoice() {
 
 // Round win
 function win() {
-  userDisplayScore.textContent = userScore++;
+  userScore++;
+  userDisplayScore.textContent = userScore;
   displayWinner.textContent = "ROUND WON";
   console.log("Round won");
+  checkGameWinner();
 }
 
 // Round lose
 function lose() {
-  computerDisplayScore.textContent = computerScore++;
+  computerScore++;
+  computerDisplayScore.textContent = computerScore;
   displayWinner.textContent = "ROUND LOST";
   console.log("Round lost");
+  checkGameWinner();
 }
 
 // Round tie
@@ -38,7 +42,7 @@ function tie() {
   console.log("Tie");
 }
 
-// Determine roundWinner
+// Determine round winner
 function roundWinner(userChoice, computerChoice) {
   if (userChoice === computerChoice) {
     tie();
@@ -53,21 +57,41 @@ function roundWinner(userChoice, computerChoice) {
   }
 }
 
-// Play a round of rps and log choices in the console
+// Check if either player has won the game
+function checkGameWinner() {
+  if (userScore === 5) {
+    displayWinner.textContent = "YOU WIN THE GAME!";
+    disableButtons();
+  } else if (computerScore === 5) {
+    displayWinner.textContent = "COMPUTER WINS THE GAME!";
+    disableButtons();
+  }
+}
+
+// Disable buttons after game is won
+function disableButtons() {
+  rockButton.disabled = true;
+  paperButton.disabled = true;
+  scissorsButton.disabled = true;
+}
+
+// Play a round of RPS and log choices in the console
 function playRound(userChoice) {
   const computerChoice = getComputerChoice();
-  console.log("User:" + userChoice);
-  console.log("Computer:" + computerChoice);
+  console.log("User: " + userChoice);
+  console.log("Computer: " + computerChoice);
   roundWinner(userChoice, computerChoice);
 }
 
-// Runs a round setting user choice to btn clicked
+// Runs a round setting user choice to button clicked
 rockButton.addEventListener("click", () => {
   playRound("rock");
 });
+
 paperButton.addEventListener("click", () => {
   playRound("paper");
 });
+
 scissorsButton.addEventListener("click", () => {
   playRound("scissors");
 });
